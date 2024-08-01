580444380853896ce58d3502e75ac87580932570
AaronRE <102276426+AaronRE@users.noreply.github.com>
2024-08-01 14:48:02 -0400

    Stubbed the login in endpoint. Installed mocha, chai, and supertest to help test my node code.

--- Website/server/routes/index.js
+++ Website/server/routes/index.js
@@ -4,8 +4,10 @@ const path = require('path');
 
 
 // Route for home page
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/public/index.html'));
-});
+//router.get('/', (req, res) => {
+//    res.sendFile(path.join(__dirname, './client/public/index.html'));
+//});
 
-module.exports = router;
+
+
+//module.exports = router;
--- Website/server/server.js
+++ Website/server/server.js
@@ -3,9 +3,21 @@ const path = require('path');
 const app = express();
 const port = 5000; // Choose a port for the server
 
+//Import router
+//const router = require('./routes/index');
+
 // Middleware to parse JSON
 app.use(express.json());
 
+// Route for home page
+//app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/public/index.html'));
+//});
+
+//login endpoint stub
+app.post('/api/login', (req, res) => {
+	res.sendStatus(200);
+});
 
 app.get('/api/endpoint', (req, res) => {
   res.json({ message: 'Hello from the Node server!' });
